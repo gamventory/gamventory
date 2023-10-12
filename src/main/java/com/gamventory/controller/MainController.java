7b9951da0913d384933a903e57b1d8b69829eff9
seouk91 <seouk91@naver.com>
2023-10-12 18:09:29 +0900

    Main Page Edit 20231012

--- src/main/java/com/gamventory/controller/MainController.java
+++ src/main/java/com/gamventory/controller/MainController.java
@@ -6,4 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
   
+    @GetMapping(value = "/")
+    public String main(){
+        return "/main";
+    }
 }
