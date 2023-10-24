5bff974a9af0c7e5578568b329cc3ba9c6915c83
hifiv12.test <hifiv12.test@gmail.com>
2023-10-24 15:16:04 +0900

    23/10/24 member basic func complete

--- src/main/java/com/gamventory/controller/MemberController.java
+++ src/main/java/com/gamventory/controller/MemberController.java
@@ -2,8 +2,7 @@ package com.gamventory.controller;
 
 import java.security.Principal;
 
-import com.gamventory.dto.MemberPasswordDto;
-import com.gamventory.dto.MemberUpdateFormDto;
+import com.gamventory.dto.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -13,8 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.gamventory.dto.MemberFIndDto;
-import com.gamventory.dto.MemberFormDto;
 import com.gamventory.entity.Member;
 import com.gamventory.service.MemberService;
 
@@ -105,18 +102,18 @@ public class MemberController {
             return "/member/memberUpdate";
         }
 
-        return "/main";
+        return "/member/memberInfo";
     }
 
     @GetMapping(value = "/find")
     public String memberFind(Model model) {
 
-        model.addAttribute("memberFindDto", MemberFIndDto.builder().build());
+        model.addAttribute("memberFindDto", MemberFindDto.builder().build());
         return "/member/memberFind";
     }
 
     @PostMapping(value = "/certification_member")
-    public String memberFound(@Valid MemberFIndDto memberFindDto, BindingResult bindingResult, Model model) {
+    public String memberFound(@Valid MemberFindDto memberFindDto, BindingResult bindingResult, Model model) {
 
         if(bindingResult.hasErrors()) {
             return "/member/find";
@@ -150,11 +147,41 @@ public class MemberController {
             return "/member/memberPasswordUpdate";
         }
 
-        Member member = memberService.getMember(principal.getName());
+        Member member = memberService.updatePassword(memberPasswordDto, principal.getName());
 
-
-        return "redirect:/member/memberInfo";
+        return "redirect:/members/info";
     }
+
+    @GetMapping(value = "/withdrawal")
+    public String deleteView(MemberDeleteForm memberDeleteForm, Model model) {
+
+        model.addAttribute("memberDeleteForm", MemberDeleteForm.builder().build());
+        return "/member/memberDelete";
+    }
+
+    @PostMapping(value = "/withdrawal")
+    public String deleteMember(@Valid MemberDeleteForm memberDeleteForm, BindingResult bindingResult,
+                               Principal principal, Model model) {
+
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("errorMessage", "비밀번호 확인 중 오류가 발생하였습니다.");
+
+        }
+
+        int result;
+
+        result = memberService.checkPassword(principal.getName(), memberDeleteForm.getPassword());
+
+        if(result == 2) {
+            model.addAttribute("checkErrorMessage", "비밀번호가 다릅니다.");
+            return "/member/memberDelete";
+        }
+
+        memberService.deleteMember(principal.getName());
+
+        return "redirect:/members/logout";
+    }
+
 
     @GetMapping(value = "/login")
     public String loginMember() {
@@ -165,7 +192,7 @@ public class MemberController {
     public String loginError(Model model) {
 
         model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해 주세요.");
-        return "member/memberLogin";
+        return "/member/memberLogin";
     }
 
 }
--- src/main/java/com/gamventory/dto/MemberUpdateFormDto.java
+++ src/main/java/com/gamventory/dto/MemberUpdateFormDto.java
@@ -17,7 +17,11 @@ public class MemberUpdateFormDto {
     @NotEmpty(message = "이메일 주소??")
     private String email;
 
-    @NotEmpty(message = "주소를 입력해 주세요.")
-    private String address;
+    @NotEmpty(message = "주소지 검색 버튼을 통해 주소를 입력해 주세요.")
+    private String zipCode;
+
+    private String streetAddress;
+
+    private String detailAddress;
 
 }
